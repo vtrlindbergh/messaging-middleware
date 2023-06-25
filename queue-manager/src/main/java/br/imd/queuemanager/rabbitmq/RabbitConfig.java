93ef16217e289f81bee127043a784a0c1394eac3
Miguel Brilhante <miguel.brilhantesilva-ext@ab-inbev.com>
2023-06-25 19:15:53 -0300

    Use Rabbit declare for build resources

--- queue-manager/src/main/java/br/imd/queuemanager/rabbitmq/RabbitConfig.java
+++ queue-manager/src/main/java/br/imd/queuemanager/rabbitmq/RabbitConfig.java
@@ -1,19 +1,17 @@
 package br.imd.queuemanager.rabbitmq;
 
 import br.imd.queuemanager.properties.ChannelProperties;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Declarables;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -24,30 +22,24 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitConfig {
   private static final Logger LOGGER = LoggerFactory.getLogger(RabbitConfig.class);
 
+  private final AmqpAdmin rabbitAdmin;
   private final ChannelProperties properties;
 
-  public RabbitConfig(ChannelProperties properties) {
+  public RabbitConfig(ChannelProperties properties, AmqpAdmin rabbitAdmin) {
     this.properties = properties;
+    this.rabbitAdmin = rabbitAdmin;
   }
 
   @Bean("genericExchange")
-  public Exchange genericExchange() {
+  public TopicExchange genericExchange() {
     LOGGER.info("Create Exchange RabbitMq: {}", properties.getGeneric().name());
     return ExchangeBuilder.topicExchange(properties.getGeneric().name()).durable(true).build();
   }
 
   @Bean("specificExchange")
-  public Exchange specificExchange() {
+  public TopicExchange specificExchange() {
     LOGGER.info("Create Exchange RabbitMq: {}", properties.getSpecific().name());
     return ExchangeBuilder.topicExchange(properties.getSpecific().name()).durable(true).build();
-  }
-
-  @Bean
-  public Declarables declarables(List<List<Queue>> queues, List<List<Binding>> bindings) {
-    return new Declarables(Stream.of(queues, bindings)
-        .flatMap(Collection::stream)
-        .flatMap(Collection::stream)
-        .collect(Collectors.toUnmodifiableSet()));
   }
 
   @Bean("genericQueues")
@@ -55,6 +47,7 @@ public class RabbitConfig {
     return properties.getGeneric().context().stream()
         .peek(queueName -> LOGGER.info("Create Queue RabbitMq: {}", queueName))
         .map(context -> QueueBuilder.durable(context).build())
+        .peek(rabbitAdmin::declareQueue)
         .toList();
   }
 
@@ -64,6 +57,7 @@ public class RabbitConfig {
       @Qualifier("genericExchange") Exchange exchange) {
     return queues.stream()
         .map(queue -> BindingBuilder.bind(queue).to(exchange).with("").noargs())
+        .peek(rabbitAdmin::declareBinding)
         .toList();
   }
 
@@ -72,6 +66,7 @@ public class RabbitConfig {
     return properties.getSpecific().context().stream()
         .peek(queueName -> LOGGER.info("Create Queue RabbitMq: {}", queueName))
         .map(context -> QueueBuilder.durable(context).build())
+        .peek(rabbitAdmin::declareQueue)
         .toList();
   }
 
@@ -81,6 +76,7 @@ public class RabbitConfig {
       @Qualifier("specificExchange") Exchange exchange) {
     return queues.stream()
         .map(queue -> BindingBuilder.bind(queue).to(exchange).with(queue.getName()).noargs())
+        .peek(rabbitAdmin::declareBinding)
         .toList();
   }
 }
